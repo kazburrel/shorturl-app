6594131da9861e769f790c66fc9198ef26633c36
kazburrel <oegbosionu@gmail.com>
2025-03-04 09:42:01 +0000

    feat: create URL controller and URL encoding service class

---
+++ app/Http/Controllers/UrlController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UrlController extends Controller
+{
+    public function encode()
+    {
+        return "Encode";
+    }
+
+    public function decode()
+    {
+        return "Decode";
+    }
+}
